c7b943f11c558703d683346aaa0446898be42a80
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2019-12-12 15:24:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
+# Bulgaria east city provider definition
 provider "aws" {
   alias  = "varna"
   region = "us-east-1"
 }
 
+# Bulgaria west city provider definition
 provider "aws" {
   alias  = "sofia"
   region = "eu-west-1"
 }
 
+# Creation of bucket in the east side of Bulgaria
 module "bucket_east" {
   source      = "./module"
   bucket_name = "georgiman-varna-city-bucket"
@@ -16,6 +19,7 @@ module "bucket_east" {
   }
 }
 
+# Creation of bucket in the west side of Bulgaria
 module "bucket_west" {
   source      = "./module"
   bucket_name = "georgiman-sofia-city-bucket"
